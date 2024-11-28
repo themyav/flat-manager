f3bc49083b05a9fc5db2f815ce8b1b8d96a78f25
vika <torichan373737@gmail.com>
2024-11-28 18:39:51 +0300

    spring base added

---
+++ src/main/java/org/example/flatmanager/FlatManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.flatmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatManagerApplication.class, args);
+	}
+
+}
