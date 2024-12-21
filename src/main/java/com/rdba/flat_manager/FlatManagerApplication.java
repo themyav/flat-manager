1dbadcd0dd1e9704c2e0bfc624e9766df3a30d5e
Kristina Kravtsova <335051@edu.itmo.ru>
2024-12-21 16:43:52 +0300

    Spring Boot project template

---
+++ src/main/java/com/rdba/flat_manager/FlatManagerApplication.java
@@ -0,0 +1,13 @@
+package com.rdba.flat_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rdba/flat_manager/FlatManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rdba.flat_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlatManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
