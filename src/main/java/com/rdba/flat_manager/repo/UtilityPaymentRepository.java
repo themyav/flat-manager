8a6eff39ad013e36091bcd369d72f300a4cfcc31
Kristina Kravtsova <335051@edu.itmo.ru>
2025-01-14 23:47:12 +0300

    Fix rebase problem

--- src/main/java/com/rdba/flat_manager/repo/UtilityPaymentRepository.java
+++ src/main/java/com/rdba/flat_manager/repo/UtilityPaymentRepository.java
@@ -6,8 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.ZonedDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface UtilityPaymentRepository extends JpaRepository<UtilityPayment, Long> {
+
+    Optional<UtilityPayment> findUtilityPaymentByUtilityId(Long id);
+
     List<UtilityPayment> findUtilityPaymentsByDateBetweenAndUtility(
             ZonedDateTime startDate,
             ZonedDateTime endDate,
