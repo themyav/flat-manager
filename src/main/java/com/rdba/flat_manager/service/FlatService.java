cd5dd68655f5a34932e42cab1e2f024805a1fa95
Kristina Kravtsova <335051@edu.itmo.ru>
2025-01-14 19:26:42 +0300

    Add method for getting utility payment for flat

--- src/main/java/com/rdba/flat_manager/service/FlatService.java
+++ src/main/java/com/rdba/flat_manager/service/FlatService.java
@@ -4,6 +4,7 @@ import com.rdba.flat_manager.dto.FlatUpdateDTO;
 import com.rdba.flat_manager.entity.Flat;
 import com.rdba.flat_manager.entity.User;
 import com.rdba.flat_manager.entity.Utility;
+import com.rdba.flat_manager.entity.UtilityPayment;
 import com.rdba.flat_manager.exception.FlatNotFound;
 import com.rdba.flat_manager.exception.UserNotFound;
 import com.rdba.flat_manager.repo.FlatRepository;
@@ -19,11 +20,13 @@ public class FlatService {
     private final FlatRepository flatRepository;
     private final UtilityService utilityService;
     private final UserRepository userRepository;
+    private final UtilityPaymentService utilityPaymentService;
 
-    public FlatService(FlatRepository flatRepository, UtilityService utilityService, UserRepository userRepository) {
+    public FlatService(FlatRepository flatRepository, UtilityService utilityService, UserRepository userRepository, UtilityPaymentService utilityPaymentService) {
         this.flatRepository = flatRepository;
         this.utilityService = utilityService;
         this.userRepository = userRepository;
+        this.utilityPaymentService = utilityPaymentService;
     }
 
     public List<Flat> getAllFlats() {
@@ -63,4 +66,12 @@ public class FlatService {
         return utilityService.getAllUtility().stream().filter(utility -> utility.getFlat().getId().equals(id)).toList();
     }
 
+    public List<UtilityPayment> getUtilityPaymentsByFlatId(Long id, String date) {
+        return utilityPaymentService.getAllUtilityPaymentsByDate(date).stream().filter(payment -> {
+            System.out.println(payment.getDate() + " " + payment.getUtility() + " " + payment.getPaymentUrl() + " " + payment.getId());
+            return payment.getId() > -1;
+        }).toList();
+    }
 }
+//payment.getUtility().getFlat().getId().equals(id)).toList();
+
