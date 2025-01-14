804802540580e6daa8db2af78fd2c029f2f34f6a
vika <torichan373737@gmail.com>
2025-01-14 17:08:05 +0300

    stats added

--- src/main/java/com/rdba/flat_manager/repo/UtilityPaymentRepository.java
+++ src/main/java/com/rdba/flat_manager/repo/UtilityPaymentRepository.java
@@ -1,10 +1,26 @@
 package com.rdba.flat_manager.repo;
 
+import com.rdba.flat_manager.entity.Utility;
 import com.rdba.flat_manager.entity.UtilityPayment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+import java.time.ZonedDateTime;
+import java.util.List;
+
 public interface UtilityPaymentRepository extends JpaRepository<UtilityPayment, Long> {
     Optional<UtilityPayment> findUtilityPaymentByUtilityId(Long id);
+    List<UtilityPayment> findUtilityPaymentsByDateBetweenAndUtility(
+            ZonedDateTime startDate,
+            ZonedDateTime endDate,
+            Utility utility
+    );
+
+
+    List<UtilityPayment> findUtilityPaymentsByDateBetweenAndUtilityIn(
+            ZonedDateTime startDate,
+            ZonedDateTime endDate,
+            List<Utility> utilities
+    );
 }
--- src/main/java/com/rdba/flat_manager/repo/UtilityRepository.java
+++ src/main/java/com/rdba/flat_manager/repo/UtilityRepository.java
@@ -1,7 +1,13 @@
 package com.rdba.flat_manager.repo;
 
+import com.rdba.flat_manager.entity.Flat;
 import com.rdba.flat_manager.entity.Utility;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UtilityRepository extends JpaRepository<Utility, Long> {
+    Utility findByFlat(Flat flat);
+
+    List<Utility> findAllByFlat(Flat flat);
 }
