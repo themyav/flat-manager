3c9e53a03113e13262ad4ab84103f03a05022998
Kristina Kravtsova <335051@edu.itmo.ru>
2025-01-15 00:53:56 +0300

    Fix phone number error + bad registration result displaying

--- frontend/flat-manager/src/pages/SignupForm.tsx
+++ frontend/flat-manager/src/pages/SignupForm.tsx
@@ -1,8 +1,7 @@
-import * as React from "react";
+import React, {useState} from "react";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import {useState} from "react";
-import { registerUser } from './api.ts'; // Import the registerUser function
+import {registerUser} from './api.ts'; // Import the registerUser function
 
 function SignupForm() {
     const [signupData, setSignupData] = useState({
@@ -12,57 +11,67 @@ function SignupForm() {
         lastName: '',
         password: '',
         confirmPassword: '',
-        phone: ''
+        phoneNumber: ''
     });
 
     const [error, setError] = useState('');
     const [successMessage, setSuccessMessage] = useState('');
     const [loading, setLoading] = useState(false);
+
     const handleSignup = async (e) => {
         e.preventDefault();
         setError('');
         setSuccessMessage('');
-        setLoading(true); // Set loading to true before the request
+        setLoading(true);
 
         const phoneRegex = /^\+7 \d{3} \d{3} \d{2} \d{2}$/;
-        if (!phoneRegex.test(signupData.phone)) {
+        if (!phoneRegex.test(signupData.phoneNumber)) {
             setError('Неверный формат номера телефона. Используйте формат: +7 XXX XXX XX XX');
-            setLoading(false); // Set loading to false if validation fails
+            setLoading(false);
             return;
         }
 
         if (signupData.password !== signupData.confirmPassword) {
             setError('Пароли не совпадают');
-            setLoading(false); // Set loading to false if validation fails
+            setLoading(false);
+            return;
+        }
+
+        if (signupData.password.length < 8) {
+            setError('Пароль должен содержать не менее 8 символов');
+            setLoading(false);
+            return;
+        }
+
+        if (!/\d/.test(signupData.password)) {
+            setError('Пароль должен содержать хотя бы одну цифру');
+            setLoading(false);
+            return;
+        }
+
+        if (!/[a-zA-Z]/.test(signupData.password)) {
+            setError('Пароль должен содержать хотя бы одну букву');
+            setLoading(false);
             return;
         }
 
         try {
             const response = await registerUser(signupData); // Use the imported function
-            if (response.status === 200) {
-                setSuccessMessage('Регистрация прошла успешно!');
-                setSignupData({
-                    username: '',
-                    email: '',
-                    firstName: '',
-                    lastName: '',
-                    password: '',
-                    confirmPassword: '',
-                    phone: ''
-                });
+            if (response.status === 201) {
+                setSuccessMessage('Регистрация прошла успешно!'); // Set success message only after successful response
             }
         } catch (error) {
-            setError('Ошибка регистрации. Попробуйте снова.');
+            setError('Ошибка регистрации: ' + error.response.data.toString() + '.\nПопробуйте снова.');
             console.error('Registration error:', error);
         } finally {
-            setLoading(false); // Set loading to false after the request completes (success or error)
+            setLoading(false);
         }
     };
 
     const handleChange = (e) => {
         const {name, value} = e.target;
 
-        if (name === "phone") {
+        if (name === "phoneNumber") {
             let formattedPhone = value;
             formattedPhone = formattedPhone.replace(/\D/g, '');
             if (formattedPhone.startsWith("7")) {
@@ -97,88 +106,97 @@ function SignupForm() {
         margin: '10px 0'
     };
 
-
     return (
-        <form onSubmit={handleSignup} style={formStyle}>
+        <>
             {error && <div style={{color: 'red', textAlign: 'center', marginBottom: '10px'}}>{error}</div>}
-            {successMessage &&
-                <div style={{color: 'green', textAlign: 'center', marginBottom: '10px'}}>{successMessage}</div>}
-            {loading &&  // Conditionally render based on boolean loading state
+            {loading &&
                 <div style={{color: 'gray', textAlign: 'center', marginBottom: '10px'}}>Подождите...</div>}
-            <TextField
-                label="Логин"
-                variant="outlined"
-                fullWidth
-                name="username"
-                value={signupData.username}
-                onChange={handleChange}
-                required
-            />
-            <TextField
-                label="Email"
-                variant="outlined"
-                fullWidth
-                name="email"
-                type="email"
-                value={signupData.email}
-                onChange={handleChange}
-                required
-            />
-            <TextField
-                label="Номер телефона"
-                variant="outlined"
-                fullWidth
-                name="phone"
-                value={signupData.phone}
-                onChange={handleChange}
-                required
-                placeholder="+7 XXX XXX XX XX"
-            />
-            <div style={passwordFieldsMargin2}/>
-            <TextField
-                label="Имя"
-                variant="outlined"
-                fullWidth
-                name="firstName"
-                value={signupData.firstName}
-                onChange={handleChange}
-            />
-            <TextField
-                label="Фамилия"
-                variant="outlined"
-                fullWidth
-                name="lastName"
-                value={signupData.lastName}
-                onChange={handleChange}
-            />
+            {successMessage && (
+                <div style={{color: 'green', textAlign: 'center', marginTop: '10px'}}>
+                    {successMessage}
+                </div>
+            )}
+            <form onSubmit={handleSignup} style={formStyle}>
 
-            <div style={passwordFieldsMargin}/>
-            <TextField
-                label="Пароль"
-                variant="outlined"
-                fullWidth
-                name="password"
-                type="password"
-                value={signupData.password}
-                onChange={handleChange}
-                required
-            />
-            <TextField
-                label="Повторите пароль"
-                variant="outlined"
-                fullWidth
-                name="confirmPassword"
-                type="password"
-                value={signupData.confirmPassword}
-                onChange={handleChange}
-                required
-            />
-            <Button type="submit" variant="contained" color="primary" disabled={loading}
-                    style={{ marginBottom: '0px' }}
-            >
-                Зарегистрироваться
-            </Button>
-        </form>
+                <TextField
+                    label="Логин"
+                    variant="outlined"
+                    fullWidth
+                    name="username"
+                    value={signupData.username}
+                    onChange={handleChange}
+                    required
+                />
+                <TextField
+                    label="Email"
+                    variant="outlined"
+                    fullWidth
+                    name="email"
+                    type="email"
+                    value={signupData.email}
+                    onChange={handleChange}
+                    required
+                />
+                <TextField
+                    label="Номер телефона"
+                    variant="outlined"
+                    fullWidth
+                    name="phoneNumber"
+                    value={signupData.phoneNumber}
+                    onChange={handleChange}
+                    required
+                    placeholder="+7 XXX XXX XX XX"
+                />
+                <div style={passwordFieldsMargin2}/>
+                <TextField
+                    label="Имя"
+                    variant="outlined"
+                    fullWidth
+                    name="firstName"
+                    value={signupData.firstName}
+                    onChange={handleChange}
+                />
+                <TextField
+                    label="Фамилия"
+                    variant="outlined"
+                    fullWidth
+                    name="lastName"
+                    value={signupData.lastName}
+                    onChange={handleChange}
+                />
+
+                <div style={passwordFieldsMargin}/>
+                <TextField
+                    label="Пароль"
+                    variant="outlined"
+                    fullWidth
+                    name="password"
+                    type="password"
+                    value={signupData.password}
+                    onChange={handleChange}
+                    required
+                    helperText={error && (error.startsWith('Пароль') ? error : '')}
+                    error={error && (error.startsWith('Пароль'))}
+                />
+                <TextField
+                    label="Повторите пароль"
+                    variant="outlined"
+                    fullWidth
+                    name="confirmPassword"
+                    type="password"
+                    value={signupData.confirmPassword}
+                    onChange={handleChange}
+                    required
+                    helperText={error && (error === 'Пароли не совпадают' ? error : '')}
+                    error={error && (error === 'Пароли не совпадают')}
+                />
+                <Button type="submit" variant="contained" color="primary" disabled={loading}
+                        style={{marginBottom: '0px'}}
+                >
+                    Зарегистрироваться
+                </Button>
+            </form>
+        </>
     );
 }
 
